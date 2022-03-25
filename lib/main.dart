15895a03b6c06b4c9b86832aa9ceefb12d9f81ea
JheimisonFLX <jheimison_007@hotmail.com>
2022-03-25 17:05:46 -0400

    Aula01 - 07

--- lib/main.dart
+++ lib/main.dart
@@ -16,48 +16,57 @@ class BytebankApp extends StatelessWidget {
         primaryColor: Colors.green[900],
         appBarTheme: AppBarTheme(color: Colors.green[900]),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Image.network('https://cdn.pixabay.com/photo/2020/08/09/14/25/business-5475660_960_720.jpg')
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                color: Theme.of(context).primaryColor,
-                width: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.people,
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Image.network('https://cdn.pixabay.com/photo/2020/08/09/14/25/business-5475660_960_720.jpg')
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              color: Theme.of(context).primaryColor,
+              width: 150,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(
                       color: Colors.white,
-                      size: 30.0,
+                      fontSize: 18.0,
                     ),
-                    Text(
-                      'Contacts',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
