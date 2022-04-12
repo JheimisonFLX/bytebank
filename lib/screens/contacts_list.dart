bfa81ea229bef389670a68da5d04eaf505eac698
JheimisonFLX <jheimison_007@hotmail.com>
2022-04-12 10:34:03 -0400

    Aula05 - 01

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -3,7 +3,12 @@ import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 import '../models/contact.dart';
 
-class ContactsList extends StatelessWidget {
+class ContactsList extends StatefulWidget {
+  @override
+  State<ContactsList> createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
   final List<Contact> contacts = [];
 
   @override
@@ -48,20 +53,10 @@ class ContactsList extends StatelessWidget {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .push(
-                MaterialPageRoute(
-                  builder: (context) => ContactForm(),
-                ),
-              )
-              .then(
-                (newContact) => debugPrint(newContact.toString()),
-              );
-        },
-        child: Icon(
-          Icons.add,
-        ),
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ContactForm()))
+            .then((value) => setState(() {})),
+        child: Icon(Icons.add),
       ),
     );
   }
